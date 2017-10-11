91bb992ebbc920eae3f82fecbdf58f0b1c6daa47
Shaun Jackman <sjackman@gmail.com>
2017-10-11 16:57:39 -0700

    longranger 2.1.6

--- longranger.rb
+++ longranger.rb
@@ -1,9 +1,8 @@
 class Longranger < Formula
   desc "10x Genomics phasing and structural variant calling pipeline"
   homepage "https://support.10xgenomics.com/genome-exome/software/downloads/latest"
-  url "https://s3-us-west-2.amazonaws.com/10x.files/releases/genome/longranger-2.1.3.tar.gz"
-  version "2.1.3"
-  sha256 "5af5025ab3450d5c234f07c26b9c5e0b74cde9c114b755d59ce4ef11c54afda9"
+  url "http://cf.10xgenomics.com/releases/genome/longranger-2.1.6.tar.gz"
+  sha256 "3e4aafc353a3d7f5cd0affd901cb779d45cca7e96ffd0b9a6a89046342cd6ade"
   # tag "bioinformatics"
 
   def install
